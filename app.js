3c0943375e07d7c7f166e5dbaf2252c0d20912d2
get <ivanchebykin4@gmail.com>
2025-11-05 17:48:44 +0500

    version v2.0.0

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-const calcBtn = document.getElementById("calcBtn")
+const calcBtn = document.querySelectorAll(".calc-item")
+const result = document.getElementById("result")
 
-calcBtn.addEventListener("click", () => {
-    const val1 = document.getElementById("input1").value
-    const val2 = document.getElementById("input2").value
-    const operation = document.getElementById("operation").value
-    if (val1 && val2 && operation) {
-        const resultDom = document.getElementById("result")
-        resultDom.innerHTML = `Результат: ${eval(`${val1} ${operation} ${val2}`)}`
+const EMPTY_VAL = "0"
+const updateRes = (val) => {
+    let result = document.getElementById("result")
+
+    if (val === "=" || val === "Enter") {
+        result.textContent = eval(result.textContent)
+    } else if (val === "RM") {
+        result.textContent = EMPTY_VAL
+    } else if (val === "⌫" || val === "Backspace") {
+        if (result.textContent && result.textContent !== "0") {
+            result.textContent = result.textContent.slice(0, -1)
+        } else {
+            result.textContent = EMPTY_VAL
+        }
+    } else {
+        result.textContent += val
+    }
+}
+
+for (let btn of calcBtn) {
+    btn.addEventListener("click", function () {
+        const val = this.textContent
+        updateRes(val)
+    })
+}
+
+document.addEventListener("keydown", function (event) {
+    const val = event.key
+    console.log(val)
+    if (!isNaN(val) || ["+", "-", "*", "/", "Enter", "Backspace"].includes(val)) {
+        updateRes(val)
     }
 })
