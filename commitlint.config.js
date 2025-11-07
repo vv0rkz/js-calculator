6a1cf90c8ec80cb8602c06050b4bde25cdeb1ffd
get <ivanchebykin4@gmail.com>
2025-11-07 22:36:49 +0500

    chore: поправлены версии

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 export default {
     extends: ["@commitlint/config-conventional"],
     rules: {
-        "type-enum": [2, "always", ["feat", "fix", "refactor", "build"]],
+        "type-enum": [2, "always", ["feat", "fix", "refactor", "build", "chore"]],
     },
 }
